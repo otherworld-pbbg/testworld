c461ebb132c73c28d326ecaac319c11b56c5af34
Ryan Otto <rotto@drew.edu>
2017-08-10 14:04:17 -0400

    Rest behavior changed to account for blood changes

--- www/cron/rest.php
+++ www/cron/rest.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*
 function getGameRoot() {
 	return "http://otherworld.loc:1234";
@@ -15,15 +15,21 @@ include_once "../root.inc.php";
 include_once getGamePath() . "/../_private/class_character.inc.php";
 include_once getGamePath() . "/../_private/abbr.inc.php";//abbreviations: para($str), ptag($tagname, $contents, [$attr])
 include_once getGamePath() . "/../_private/conn.inc.php";
+include_once getGamePath() . "/../_private/class_obj.inc.php";
+include_once getGamePath() . "/../_private/constants.php";
+
 
 function getLiveCharacters($mysqli) {
 	$retArr = array();
 	$sql = "SELECT `uid` FROM `chars` WHERE `status`=1 ORDER BY `uid`";
 	$result = $mysqli->query($sql);
+	$rowcounter=0;
 	if (mysqli_num_rows($result)) {
 		while ($row = mysqli_fetch_row($result)) {
 			$retArr[] = $row[0];
+            $rowcounter++;
 		}
+		echo "Row count: " . $rowcounter;
 		return $retArr;
 	}
 	else return false;		
@@ -34,20 +40,32 @@ function doLoop($mysqli) {
 	if (!$chars) return 0;
 	$counter = 0;
 	$counter2 = 0;
+	$blood_gained_counter=0;
 	foreach ($chars as $ci) {
-		$c = new Character($mysqli, $ci);
-		$hasChanged=$c->advanceAge();
+        $c = new Character($mysqli, $ci);
+
+        $bo = new Obj($mysqli, $c->bodyId);
+		$currentBlood = $bo->getAttribute(ATTR_BLOOD);
+		$maxBlood = $bo->weight*0.1;
+        if($currentBlood != $maxBlood) $blood_gained_counter++; //If this character will gain blood, increment counter
+        $missingBloodPercentage = $maxBlood - ($currentBlood/$maxBlood) * 100;
+        if($missingBloodPercentage <= 10 || $missingBloodPercentage > 40) $regen = HEAL_BLOOD_GRAMS;
+        else $regen = HEAL_BLOOD_BONUS*($missingBloodPercentage-10) + HEAL_BLOOD_GRAMS;
+        $newBlood = min($currentBlood+$regen, $maxBlood);
+        $bo->setAttribute(ATTR_BLOOD, $newBlood);
+
+		$hasChanged = $c->advanceAge();
 		if($hasChanged) $counter2++;
 		
 		$result = $c->rest_auto();
 		if ($result>0) $counter++;
-	}
+        echo $c->uid . " gained " . $newBlood . " grams of blood.<br/>";
+    }
 	echo $counter2 . " people advanced to a new age/weight category";
-	return $counter;
+    return $counter;
 }
 
 $result = doLoop($mysqli);
-
 echo $result;
 
 ?>
