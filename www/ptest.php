589cb1317f95268e4bc46e1493bacabf01b487f8
Ilona Ward <30551686+Seeeks@users.noreply.github.com>
2017-09-16 22:08:56 +0300

    Minor test changes

--- www/ptest.php
+++ www/ptest.php
@@ -21,17 +21,21 @@ if (isset($_GET["smooth"])) {
 }
 else $smooth = rand(10,90);
 
-if (isset($_GET["lift"])) {
-	$tilt = $_GET["lift"];
-	if ($tilt!="n"&&$tilt!="s"&&$tilt!="e"&&$tilt!="w"&&$tilt!="ne"&&$tilt!="nw"&&$tilt!="se"&&$tilt!="sw") $tilt = NULL;
+if (isset($_GET["x"])) {
+	$xoff = setBint($_GET["x"], 0, 100000000, 0);
 }
-else $tilt = NULL;
+else $xoff = 0;
+
+if (isset($_GET["y"])) {
+	$yoff = setBint($_GET["y"], 0, 10000, 0);
+}
+else $yoff = 0;
 
 $bob = new Perlin($seed);
 $bill = new Perlin($seed+1);
 
 $space = 70;
-$gridsize = 30;
+$gridsize = 20;
 
 //echo "smooth: " . $smooth . "<br>";
 //echo "seed: " . $seed. "<br>";
@@ -41,48 +45,45 @@ $materials = array(
 	"gras",
 	"fer",
 	"sub",
-	"moss"
+	"mos"
 	);
+
+$counter = 0;
+$ranges = array();
+
+for ($i = 0; $i<sizeof($materials); $i++) {
+	$counter += rand(5,25);
+	if ($i==sizeof($materials)-1) $ranges[] = 101;
+	else $ranges[] = $counter;
+}
 
 $material = $materials[rand(0,sizeof($materials)-1)];
 
 $array = array();
 for($y=0; $y<$gridsize; $y+=1) {
 	for($x=0; $x<$gridsize; $x+=1) {
-		$num = $bob->noise($x,$y,0,$smooth);
-		$num2 = $bill->noise($x,$y,0,20);
+		$num = $bob->noise($x+$xoff,$y+$yoff,0,$smooth);
+		$num2 = $bill->noise($x+$xoff,$y+$yoff,0,20);
 		
 		$raw = ($num/2)+.5;
 		$raw2 = ($num2/2)+.5;
-		if ($tilt=="n"||$tilt=="ne"||$tilt=="nw") {
-			if ($y<($gridsize/2)) {
-				$raw += (($gridsize/2)-$y)/$gridsize;
-			}
-		}
-		if ($tilt=="s"||$tilt=="se"||$tilt=="sw") {
-			if ($y>($gridsize/2)) {
-				$raw += ($y-($gridsize/2))/$gridsize;
-			}
-		}
-		if ($tilt=="w"||$tilt=="nw"||$tilt=="sw") {
-			if ($x<($gridsize/2)) {
-				$raw += (($gridsize/2)-$x)/$gridsize;
-			}
-		}
-		if ($tilt=="e"||$tilt=="ne"||$tilt=="se") {
-			if ($x>($gridsize/2)) {
-				$raw += ($x-($gridsize/2))/$gridsize;
-			}
-		}
-		
+
 		if ($raw < 0) $raw = 0;
 		if ($raw > 1) $raw = 1;
 		if ($raw2 < 0) $raw2 = 0;
 		if ($raw2 > 1) $raw2 = 1;
 		
-		$raw2 = round($raw2*5);
+		$raw2 = round($raw2*100);
 		
-		$material = $materials[$raw2];
+		$r2 = 0;
+		foreach ($ranges as $key => $r) {
+			if ($r>$raw2) {
+				$r2 = $key;
+				break;
+			}
+		}
+		
+		$material = $materials[$r2];
 		
 		$array[] = array(
 			"x" => $x,
@@ -93,7 +94,7 @@ for($y=0; $y<$gridsize; $y+=1) {
 	}
 }
 
-$bottom = imagecreatetruecolor($gridsize*$space+100,$gridsize*$space);
+$bottom = imagecreatetruecolor($gridsize*$space,$gridsize*$space-120);
 //$color = imagecolorallocate($bottom, 50,50,30);
 //imagefill ($bottom, 0, 0, $color);
 
@@ -102,7 +103,7 @@ $variation = 100;
 foreach ($array as $info) {
 	$d = round($info["raw"]*$variation-($variation*0.6));
 	
-	$bottom = applyStamp($bottom, 'graphics/' . $info["material"] . '1.png', $info["x"]*$space+50, round(150+$info["y"]*$space-($info["raw"]*$variation*2.5)), $d);
+	$bottom = applyStamp($bottom, 'graphics/' . $info["material"] . '1.png', $info["x"]*$space+($info["y"]*2)-60, round(50+$info["y"]*$space-($info["raw"]*$variation*2.5)-($info["x"]*2)), $d);
 	$prevy = $info["y"];
 }
 
